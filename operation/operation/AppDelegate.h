3ffcf5d08bf5f3ab2ade4beb85ca10d13deef1b8
LeeMinglu <402007353@qq.com>
2016-01-03 15:47:49 +0800

    项目初始化

---
+++ operation/operation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  operation
+//
+//  Created by 李明禄 on 16/1/3.
+//  Copyright © 2016年 SocererGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ operation/operation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  operation
+//
+//  Created by 李明禄 on 16/1/3.
+//  Copyright © 2016年 SocererGroup. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
